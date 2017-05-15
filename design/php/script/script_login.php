b07ae86866061697447c7483f272d65a8fffe3ac
SHRipper <lukas.schaef@gmx.de>
2017-05-15 22:29:29 +0200

    login with wrong pw now results in error

--- design/php/script/script_login.php
+++ design/php/script/script_login.php
@@ -13,39 +13,41 @@ if ($_GET["username"] == '' or $_GET["password"] == '') {
     // debug connection to local database
     $pdo = new PDO("mysql:dbname=thwitterdb;host=localhost", "root", "");
 
-    $sql = "SELECT password FROM User where lower(username) = ?";
+    $sql = "SELECT password FROM User WHERE lower(username) = ?";
     $statement = $pdo->prepare($sql);
     if ($statement->execute(array(strtolower($username)))) {
 
-        if ($statement->rowCount() == 0){
+        if ($statement->rowCount() == 0)
             return_with_error();
-        }else {
-            // data exists for given username
-            $row = $statement->fetch();
-            debug("statement correct. row: " . $row['password']);
-            if ($row['password'] == $password) {
-                echo "<script>window.location='/THwitter/design/php/main/main.php'</script>";
-            } else {
-                echo "<script>window.location='/THwitter/design/php/login/login.php'</script>";
-            }
+
+        // data exists for given username
+        $row = $statement->fetch();
+        debug("statement correct. row: " . $row['password']);
+        if ($row['password'] == $password) {
+            echo "<script>window.location='/THwitter/design/php/main/main.php'</script>";
+        } else {
+            return_with_error();
         }
+
         $statement->free();
-    }else{
+    } else {
         // error while executing the statement
         debug("error my frieeend");
     }
     die();
 }
 
-function debug( $data ) {
+function debug($data)
+{
     $output = $data;
-    if ( is_array( $output ) )
-        $output = implode( ',', $output);
+    if (is_array($output))
+        $output = implode(',', $output);
 
     echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
 }
 
-function return_with_error(){
+function return_with_error()
+{
     echo "<script>
 		window.location='/THwitter/design/php/login/login.php?err=true';
 		</script>";
