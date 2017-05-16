4371a1f7930159f233ccf38aba585169b8204482
walterda64090 <walterda64090@th-nuernberg.de>
2017-05-16 11:17:46 +0200

    Einfügen eines 2-Dim. Array für Sprachauswahl und übernehmen des Arrays in login.php (Auswahl der Sprachen über Links fehlt noch)

--- design/php/login/login.php
+++ design/php/login/login.php
@@ -4,12 +4,14 @@
 	<title>Anmelden</title>
 	<link rel="stylesheet" type="text/css" href="../../style/login_registration_style.css">
 	<script src="/THwitter/design/js/validate.js"></script>
-
 </head>
 
 <body>
 	<header id="login_header">
-
+        <?php
+        include 'script_language.php';
+        $language = "german";
+        ?>
 		<section class="tile" id="section_language">
 			<a href="" class="inactive_link" id="language_link">Language</a>
 			<ul id="ul_language">
@@ -28,7 +30,10 @@
 			</ul>
 		</section>
 		<section  class="tile" id="section_about">
-			<a href="#" id="about_link">&Uuml;ber</a>
+            <a href="#" id="about_link">
+                <?php
+                echo $choose_language[$language]["about"];
+                ?></a>
 		</section>
 		
 		<section id="header_wrapper">
@@ -42,14 +47,22 @@
 	<section id="header_shadow"></section>
 	<main>
 		<section id="welcome_text_wrapper">
-			<h2 id="welcome_text">Melde dich an und beginne deine Reise</h2>
+			<h2 id="welcome_text">
+                <?php
+                echo $choose_language[$language]["welcome"];
+                ?>
+            </a></h2>
 		</section>
 		
 		<section id="login_section">
 			<form action="../script/script_login.php" id="login_form" method="get">
 				<ul>
                     <li>
-                        <label id="input_error_notify">Die Anmeldedaten stimmen nicht überein!</label>
+                        <label id="input_error_notify">
+                            <?php
+                            echo $choose_language[$language]["input_error"];
+                            ?>
+                        </label>
                     </li>
 					<li>							
 						<input id="input_username" class="textbox" placeholder="username" name="username" type="text">
@@ -58,9 +71,21 @@
 						<input id="input_password" class="textbox" placeholder="password" name="password" type="password">
 					</li>
 					<li id="section_login_register">
-                        <a class="login_button" onclick="document.getElementById('login_form').submit();">Anmelden</a>
+                        <a class="login_button" onclick="document.getElementById('login_form').submit();">
+                            <?php
+                            echo $choose_language[$language]["login_button"];
+                            ?>
+                        </a>
                         <section id="register_section">
-                            <p>oder <a class="register_link" href="../registration/registration.php">registrieren</a>
+                            <p>
+                                <?php
+                                echo $choose_language[$language]["or"];
+                                ?>
+                                <a class="register_link" href="../registration/registration.php">
+                                    <?php
+                                    echo $choose_language[$language]["register_link"];
+                                    ?>
+                                </a>
                             </p>
                         </section>
                         <!-- invisible button to submit the form on return click -->
