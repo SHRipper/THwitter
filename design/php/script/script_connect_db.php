889bf51b9195f1246b856caa8b83478b4b084866
SHRipper <lukas.schaef@gmx.de>
2017-05-23 10:01:29 +0200

    changed db connect to duck.servebeer.com

--- design/php/script/script_connect_db.php
+++ design/php/script/script_connect_db.php
@@ -1,4 +1,7 @@
 <?php
 
 // connection to use when running on the server
-$pdo = $pdo = new PDO("mysql:dbname=THwitterDB;host=localhost;port=3306", "RubberDuck", "WebProg");
+//$pdo = $pdo = new PDO("mysql:dbname=THwitterDB;host=localhost;port=3306", "RubberDuck", "WebProg");
+
+// connection to use when testing in class
+$pdo = $pdo = new PDO("mysql:dbname=THwitterDB;host=duck.servebeer.com;port=3306", "RubberDuck", "WebProg");
