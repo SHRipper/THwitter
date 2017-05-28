3323dfe66d87295919a8ed4e90dd95bba7e864ec
Tim <tim.vogel227@gmail.com>
2017-05-28 22:14:29 +0200

    fix german translation for user entered wrong credentials

--- design/php/script/script_language.php
+++ design/php/script/script_language.php
@@ -2,7 +2,7 @@
 //Deutsch
 $choose_language["german"]["about"] = "Über";
 $choose_language["german"]["welcome"] = "Melde dich an und beginne deine Reise";
-$choose_language["german"]["input_error"] = "Die Anmeldedaten stimmen nicht überein!";
+$choose_language["german"]["input_error"] = "Die Anmeldedaten stimmen nicht!";
 $choose_language["german"]["login_button"] = "Anmelden";
 $choose_language["german"]["or"] = "oder";
 $choose_language["german"]["register_link"] = "registrieren";
