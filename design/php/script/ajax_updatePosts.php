ffbd26ab242da1f1c8db782652682cbaa2ef9cf2
Tim <tim.vogel227@gmail.com>
2017-06-20 10:56:08 +0200

    only send unread posts to client in ajaxUpdatePosts

--- design/php/script/ajax_updatePosts.php
+++ design/php/script/ajax_updatePosts.php
@@ -2,24 +2,28 @@
 session_start();
 include '../script/script_connect_db.php';
 
+$highestId = -1;
+if (isset($_SESSION['lastPostId'])) {
+    $highestId = $_SESSION['lastPostId'];
+}
 $sql = "SELECT post_id AS id, message AS message, username AS author, DATE_FORMAT(DATE_ADD(timestamp, INTERVAL 2 HOUR), '%d.%m.%Y %H:%i') AS time
             FROM Post post
             JOIN User author ON post.sender_id = author.user_id
+            WHERE post_id > ?
             ORDER BY timestamp DESC";
 
 $statement = $pdo->prepare($sql);
-$statement->execute();
-//TODO: Display posts longer than one line
+$statement->execute(array($highestId));
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$highestId = -1;
-foreach($result AS $post) {
+foreach ($result AS $post) {
     if ($post['id'] > $highestId) {
         $highestId = $post['id'];
     }
 }
 $_SESSION['lastPostId'] = $highestId;
 
-$JSON=json_encode($result);
+$JSON = json_encode($result);
 echo $JSON;
+
 ?>
