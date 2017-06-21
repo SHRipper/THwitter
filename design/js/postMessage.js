4ac26b0d7342410aab922313695f47aa6fb34962
Daniel <walterda64090@th-nuernberg>
2017-06-21 10:04:35 +0200

    Ajax-Skript umbenannt

--- design/js/postMessage.js
+++ design/js/postMessage.js
@@ -3,7 +3,7 @@ function sendNewPost() {
         let newPost = document.getElementById('inputText').value;
         let reqObj = {method: "post", m: newPost};
         let req = new XMLHttpRequest();
-        req.open("POST", "../script/asyncReceivePost.php", true);
+        req.open("POST", "../script/ajax_newPost.php", true);
         req.onreadystatechange = () => {
             if (req.readyState === 4) {
                 if (!(req.status >= 200 && req.status < 300)) {
